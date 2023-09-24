14faae02689cdf824619b25a4b8e7f7abe2e9957
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-09-25 00:03:34 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
+export default function loginPage() {
+  const [user, sertUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="email"
+        value={user.email}
+        onChange={(e) => sertUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        value={user.password}
+        onChange={(e) => sertUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login
+      </button>
+      <Link href="/signup">Visit signup page</Link>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
+export default function SignupPage() {
+  const [user, sertUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        value={user.username}
+        onChange={(e) => sertUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="email"
+        value={user.email}
+        onChange={(e) => sertUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        value={user.password}
+        onChange={(e) => sertUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Signup
+      </button>
+      <Link href="/login">Visit login page</Link>
+    </div>
+  );
+}
